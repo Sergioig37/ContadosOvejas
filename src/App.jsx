a66016a0bdef72ecf48523b1d3857cd4ee017c55
Sergioig37 <sergioig37@educastur.es>
2024-01-26 12:26:15 +0100

    Hecho, se puede mejorar (?)

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { SheepList } from "./SheepList";
+
+export const App = () => {
+
+  
+  const [listaOvejas, setListaOvejas] = useState([]);
+
+  const addOveja = () => {
+    setListaOvejas([...listaOvejas, "https://www.clker.com/cliparts/e/4/8/7/13280460782141411990Cartoon%20Sheep.svg.hi.png"]);
+  };
+
+  return (
+    <>
+      <h3 className="title">Contador de ovejas</h3>
+
+      <div className="wrapper">
+        <h4 className="counter">{listaOvejas.length}</h4>
+        <button className="button" onClick={addOveja}>Añadir oveja</button>
+        <SheepList ovejas={listaOvejas} />
+      </div>
+    </>
+  );
+};
---
+++ src/SheepList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export const SheepList = ({ovejas}) => {
+  
+
+    
+
+  return (
+    <>
+    {/**Cuidadín, el elemento después de la coma es el index, si solo es un elemento va a pillar el primer elemento del map*/}
+      {ovejas.map((oveja, index) => {
+        {/**C*/}
+        return <div key={index}>
+            <img src={oveja} alt="" />
+        </div>;
+      })}
+    </>
+  );
+};
