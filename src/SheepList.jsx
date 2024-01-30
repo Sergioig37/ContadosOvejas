2b8f0ff4cf321de648b3e78d24821dec02887627
Sergioig37 <sergioig37@educastur.es>
2024-01-30 08:51:39 +0100

    ahora ya se pinta mejor

--- src/SheepList.jsx
+++ src/SheepList.jsx
@@ -9,12 +9,15 @@ export const SheepList = ({ovejas}) => {
   return (
     <>
     {/**Cuidadín, el elemento después de la coma es el index, si solo es un elemento va a pillar el primer elemento del map*/}
-      {ovejas.map((oveja, index) => {
-        {/**C*/}
-        return <div key={index}>
-            <img src={oveja} className="sheep__img" alt="" />
-        </div>;
+
+
+    <ul className="image__list">
+      {ovejas.map((oveja, index) => { 
+        return <li key={index}>
+          <img src={oveja} className="sheep__img" alt="" />
+        </li>
       })}
+    </ul>
     </>
   );
 };
